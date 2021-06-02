701315a044260f3e9c5952f5b6a34cef970983c0
Jiang-LiWen <85112455+Jiang-LiWen@users.noreply.github.com>
2021-06-02 19:44:32 +0800

    第一次测试

--- tsm/src/main/java/com/trkj/tsm/TsmApplication.java
+++ tsm/src/main/java/com/trkj/tsm/TsmApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TsmApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(TsmApplication.class, args);
     }
