3ad070c3d9234c534d20563fa7ffb0e82f86604d
Jiang-LiWen <85112455+Jiang-LiWen@users.noreply.github.com>
2021-06-03 14:51:46 +0800

    Update TsmApplication.java
    修改

--- tsm/src/main/java/com/trkj/tsm/TsmApplication.java
+++ tsm/src/main/java/com/trkj/tsm/TsmApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TsmApplication {
-
-    
     public static void main(String[] args) {
         SpringApplication.run(TsmApplication.class, args);
     }
