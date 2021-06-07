644c284ca1201b449bcd357a13e2a7e7365f65a0
Jiang-LiWen <85112455+Jiang-LiWen@users.noreply.github.com>
2021-06-07 14:18:52 +0800

    公告

--- tsm/src/main/java/com/trkj/tsm/vo/AnnouncementVo.java
+++ tsm/src/main/java/com/trkj/tsm/vo/AnnouncementVo.java
@@ -37,13 +37,11 @@ public class AnnouncementVo implements Serializable {
     /**
      * 开始时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
 
     /**
      * 结束时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
     /**
