d11a255c80accff120e3f3376bdb9cfd2f439d79
mmzsit <652169851@163.com>
2021-06-16 20:06:30 +0800

    教务学员交接审批（交接修改、学员表新增）

---
+++ tsm/src/main/java/com/trkj/tsm/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.trkj.tsm.service;
+
+import com.trkj.tsm.entity.Student;
+import com.trkj.tsm.vo.StudentVo;
+
+import java.util.List;
+
+public interface StudentService {
+    int insertStudent(Student student);//添加学员
+
+    List<StudentVo> selectStudentLike(String likeke);//页面显示加分页查询
+
+    StudentVo updateStudent(StudentVo studentVo);//修改学员
+
+    StudentVo deleteStudent(StudentVo studentVo);//删除学员
+}
