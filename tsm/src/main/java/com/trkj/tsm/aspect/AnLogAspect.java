40242e8c30ce2e31cadaee4d21dec73209b6badd
Jiang-LiWen <85112455+Jiang-LiWen@users.noreply.github.com>
2021-06-28 23:16:24 +0800

    切面

--- tsm/src/main/java/com/trkj/tsm/aspect/AnLogAspect.java
+++ tsm/src/main/java/com/trkj/tsm/aspect/AnLogAspect.java
@@ -22,51 +22,51 @@ public class AnLogAspect{
     @Pointcut("execution(* com.trkj.tsm.service..*.*(..))")
     public void x(){
     }
-//    @Around("x()")
-//    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-//        log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>即将进入到{}类的{}方法",joinPoint.getTarget().getClass().getName(),joinPoint.getSignature());
-//        Object obj = joinPoint.proceed();
-//        Object[] args = joinPoint.getArgs();
-//        String className = joinPoint.getSignature().getName().substring(0,3);
-//        if(className.equals("add")){
-//            Class faqVoClass = args[0].getClass();
-//            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
-//            String tableName = fieldMeta.className();
-//            String addName =  args[0].getClass().getDeclaredField("addname").get(args[0]).toString();
-//            Opjournal opjournal = new Opjournal();
-//            opjournal.setEmpName(addName);
-//            opjournal.setOpcontent("添加"+tableName+"数据");
-//            opJournalService.addOpJournal(opjournal);
-//        }else if(className.equals("upd")){
-//            Class faqVoClass = args[0].getClass();
-//            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
-//            String tableName = fieldMeta.className();
-//            String updateName =  args[0].getClass().getDeclaredField("updatename").get(args[0]).toString();
-//            Opjournal opjournal = new Opjournal();
-//            opjournal.setEmpName(updateName);
-//            opjournal.setOpcontent("修改"+tableName+"数据");
-//            opJournalService.addOpJournal(opjournal);
-//        }else if(className.equals("del")){
-//            Class faqVoClass = args[0].getClass();
-//            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
-//            String tableName = fieldMeta.className();
-//            String deleteName =  args[0].getClass().getDeclaredField("deletename").get(args[0]).toString();
-//            Opjournal opjournal = new Opjournal();
-//            opjournal.setEmpName(deleteName);
-//            opjournal.setOpcontent("删除"+tableName+"数据");
-//            opJournalService.addOpJournal(opjournal);
-//        }else if(className.equals("app")){
-//            Class faqVoClass = args[0].getClass();
-//            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
-//            String tableName = fieldMeta.className();
-//            String deleteName =  args[0].getClass().getDeclaredField("appname").get(args[0]).toString();
-//            Opjournal opjournal = new Opjournal();
-//            opjournal.setEmpName(deleteName);
-//            opjournal.setOpcontent("审核"+tableName+"数据");
-//            opJournalService.addOpJournal(opjournal);
-//        }
-//        log.debug("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<完成{}类的{}方法执行",joinPoint.getTarget().getClass().getName(),joinPoint.getSignature());
-//        return obj;
-//    }
+    @Around("x()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>即将进入到{}类的{}方法",joinPoint.getTarget().getClass().getName(),joinPoint.getSignature());
+        Object obj = joinPoint.proceed();
+        Object[] args = joinPoint.getArgs();
+        String className = joinPoint.getSignature().getName().substring(0,3);
+        if(className.equals("add")){
+            Class faqVoClass = args[0].getClass();
+            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
+            String tableName = fieldMeta.className();
+            String addName =  args[0].getClass().getDeclaredField("addname").get(args[0]).toString();
+            Opjournal opjournal = new Opjournal();
+            opjournal.setEmpName(addName);
+            opjournal.setOpcontent("添加"+tableName+"数据");
+            opJournalService.addOpJournal(opjournal);
+        }else if(className.equals("upd")){
+            Class faqVoClass = args[0].getClass();
+            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
+            String tableName = fieldMeta.className();
+            String updateName =  args[0].getClass().getDeclaredField("updatename").get(args[0]).toString();
+            Opjournal opjournal = new Opjournal();
+            opjournal.setEmpName(updateName);
+            opjournal.setOpcontent("修改"+tableName+"数据");
+            opJournalService.addOpJournal(opjournal);
+        }else if(className.equals("del")){
+            Class faqVoClass = args[0].getClass();
+            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
+            String tableName = fieldMeta.className();
+            String deleteName =  args[0].getClass().getDeclaredField("deletename").get(args[0]).toString();
+            Opjournal opjournal = new Opjournal();
+            opjournal.setEmpName(deleteName);
+            opjournal.setOpcontent("删除"+tableName+"数据");
+            opJournalService.addOpJournal(opjournal);
+        }else if(className.equals("app")){
+            Class faqVoClass = args[0].getClass();
+            ClassMeta fieldMeta = (ClassMeta)faqVoClass.getAnnotation(ClassMeta.class);
+            String tableName = fieldMeta.className();
+            String deleteName =  args[0].getClass().getDeclaredField("appname").get(args[0]).toString();
+            Opjournal opjournal = new Opjournal();
+            opjournal.setEmpName(deleteName);
+            opjournal.setOpcontent("审核"+tableName+"数据");
+            opJournalService.addOpJournal(opjournal);
+        }
+        log.debug("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<完成{}类的{}方法执行",joinPoint.getTarget().getClass().getName(),joinPoint.getSignature());
+        return obj;
+    }
 }
 
